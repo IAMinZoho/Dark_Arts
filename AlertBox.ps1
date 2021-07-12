4e80c34f560e051461bf7738b314d9d91dd8087e
cha0S <84743508+IAMinZoho@users.noreply.github.com>
2021-07-12 08:21:14 +0530

    Update AlertBox.ps1

--- AlertBox.ps1
+++ AlertBox.ps1
@@ -2,7 +2,7 @@ Add-Type -AssemblyName PresentationCore,PresentationFramework
 $ButtonType = [System.Windows.MessageBoxButton]::Ok
 $MessageIcon = [System.Windows.MessageBoxImage]::Exclamation
 $MessageBody = "You can run but you can't HIDE!"
-$MessageTitle = "***ALERT - Contoso IT Admin***"
+$MessageTitle = "***ALERT - IT Admin***"
 
 $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle, $ButtonType,$MessageIcon)
 
