79623f8e73fe4a5b4d87d224ae8655a4781c2e53
cha0S <84743508+IAMinZoho@users.noreply.github.com>
2021-07-12 08:18:03 +0530

    Add files via upload

---
+++ AlertBox.ps1
@@ -0,0 +1,9 @@
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$ButtonType = [System.Windows.MessageBoxButton]::Ok
+$MessageIcon = [System.Windows.MessageBoxImage]::Exclamation
+$MessageBody = "You can run but you can't HIDE!"
+$MessageTitle = "***ALERT - Contoso IT Admin***"
+
+$Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle, $ButtonType,$MessageIcon)
+
+Write-Host "Your choice is $Result"
